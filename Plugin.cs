b0a34340cca2333e67d98696a67d5a438261c134
ButteryStancakes <jlj3322@gmail.com>
2024-06-12 14:19:19 -0400

    Bump version number

--- Plugin.cs
+++ Plugin.cs
@@ -25,7 +25,7 @@ namespace ButteryFixes
     [BepInDependency("e3s1.BetterLadders", BepInDependency.DependencyFlags.SoftDependency)]
     public class Plugin : BaseUnityPlugin
     {
-        const string PLUGIN_GUID = "butterystancakes.lethalcompany.butteryfixes", PLUGIN_NAME = "Buttery Fixes", PLUGIN_VERSION = "1.3.0";
+        const string PLUGIN_GUID = "butterystancakes.lethalcompany.butteryfixes", PLUGIN_NAME = "Buttery Fixes", PLUGIN_VERSION = "1.3.1";
         internal static new ManualLogSource Logger;
 
         internal static bool DISABLE_LADDER_PATCH;
