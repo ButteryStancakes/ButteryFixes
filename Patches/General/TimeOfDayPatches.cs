46829be3b22dc0f855683d627ba9596b335e1f0b
ButteryStancakes <jlj3322@gmail.com>
2025-01-22 07:02:10 -0500

    Fix music not playing

---
+++ Patches/General/TimeOfDayPatches.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ButteryFixes.Patches.General
+{
+    [HarmonyPatch]
+    internal class TimeOfDayPatches
+    {
+        [HarmonyPatch(typeof(TimeOfDay), nameof(TimeOfDay.PlayTimeMusicDelayed))]
+        [HarmonyPrefix]
+        static void PostPredictAllOutsideEnemies(TimeOfDay __instance, ref Coroutine ___playDelayedMusicCoroutine)
+        {
+            // allow music to play again, unless visiting Gordion multiple times in a row
+            if (StartOfRound.Instance.currentLevel.name != "CompanyBuildingLevel" && !__instance.TimeOfDayMusic.isPlaying && !SoundManager.Instance.musicSource.isPlaying)
+                ___playDelayedMusicCoroutine = null;
+        }
+    }
+}
